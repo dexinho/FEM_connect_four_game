0fdd8045344ec84fe2ed5ee8d828ff29d1d88afb
Jasmin Hidanovic <130430763+dexinho@users.noreply.github.com>
2023-09-07 22:44:40 +0200

    improved the way holes are clicked

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const fourthColumn = document.querySelectorAll('.fourth-column')
 const fifthColumn = document.querySelectorAll('.fifth-column')
 const sixthColumn = document.querySelectorAll('.sixth-column')
 const seventhColumn = document.querySelectorAll('.seventh-column')
-const insideTheHole = document.querySelectorAll('.inside-the-hole')
+const holes = document.querySelectorAll('.hole')
 const timerCountSpan = document.querySelector('.timer-count')
 const playerTurnSpan = document.querySelector('.player-turn')
+const endingDialog = document.querySelector('#ending-dialog')
+const whoWonSpan = document.querySelector('#who-won-span')
+let PLAYER_WHO_WON = null
 
 const PLAY_MATRIX = []
 const ALL_COLUMNS_ARR = [firstColumn, secondColumn, thirdColumn, fourthColumn, fifthColumn, sixthColumn, seventhColumn]
@@ -17,12 +20,12 @@ let PLAYER_TWO_COLOR = 'yellow'
 let CURRENT_COLOR = true
 const TIMEOUT_IDS = []
 
-let MAX_SECONDS_PER_MOVE = 10
+let MAX_SECONDS_PER_MOVE = 1
 
 function createMatrix() {
     const randomArr = []
     for (let index = 0; index < 9; index++) {
-        randomArr.push(Array.from({ length: 10 }, (el, index) => Math.random() * 1000))
+        randomArr.push(Array.from({ length: 10 }, (el, index) => Math.random() * 10000))
     }
 
     return randomArr
@@ -52,9 +55,9 @@ function delay(ms) {
 function startTimer() {
     let counter = MAX_SECONDS_PER_MOVE
     function updateTimer() {
-        if (counter >= 0) {
+        if (counter > 0) {
             TIMEOUT_IDS.push(setTimeout(updateTimer, 1000))
-            timerCountSpan.innerText = counter--
+            timerCountSpan.innerText = --counter
         }
         else if (ALL_COLUMNS_ARR.length > 0) {
             let randomIndex = Math.floor(Math.random() * ALL_COLUMNS_ARR.length)
@@ -70,42 +73,36 @@ function clearTimeoutIDs() {
 }
 
 function checkIfThereIsAWinner() {
-    let playerWhoWon = null
-    let wonAtRow = null
 
-    outerLoop: for (let i = 0; i < PLAY_MATRIX.length - 3; i++) {
+    for (let i = 0; i < PLAY_MATRIX.length - 3; i++) {
         for (let j = 0; j < PLAY_MATRIX[0].length - 3; j++) {
             if (PLAY_MATRIX[i][j] === PLAY_MATRIX[i + 1][j]
                 && PLAY_MATRIX[i][j] === PLAY_MATRIX[i + 2][j]
                 && PLAY_MATRIX[i][j] === PLAY_MATRIX[i + 3][j]) {
-                playerWhoWon = PLAY_MATRIX[i][j]
-                break outerLoop
+                PLAYER_WHO_WON = PLAY_MATRIX[i][j]
+                return true
             }
             else if (PLAY_MATRIX[i][j] === PLAY_MATRIX[i][j + 1]
                 && PLAY_MATRIX[i][j] === PLAY_MATRIX[i][j + 2]
                 && PLAY_MATRIX[i][j] === PLAY_MATRIX[i][j + 3]) {
-                playerWhoWon = PLAY_MATRIX[i][j]
-                break outerLoop
+                PLAYER_WHO_WON = PLAY_MATRIX[i][j]
+                return true
             }
             else if (PLAY_MATRIX[i][j] === PLAY_MATRIX[i + 1][j - 1]
                 && PLAY_MATRIX[i][j] === PLAY_MATRIX[i + 2][j - 2]
                 && PLAY_MATRIX[i][j] === PLAY_MATRIX[i + 3][j - 3]) {
-                playerWhoWon = PLAY_MATRIX[i][j]
-                break outerLoop
+                PLAYER_WHO_WON = PLAY_MATRIX[i][j]
+                return true
             }
             else if (PLAY_MATRIX[i][j] === PLAY_MATRIX[i + 1][j + 1]
                 && PLAY_MATRIX[i][j] === PLAY_MATRIX[i + 2][j + 2]
                 && PLAY_MATRIX[i][j] === PLAY_MATRIX[i + 3][j + 3]) {
-                playerWhoWon = PLAY_MATRIX[i][j]
-                break outerLoop
+                PLAYER_WHO_WON = PLAY_MATRIX[i][j]
+                return true
             }
         }
     }
-
-    console.log('player who won: ', playerWhoWon)
 }
-
-// setTimeout(checkIfThereIsAWinner, 12000);
 
 function addCircle(column, matrixIndex) {
     clearTimeoutIDs()
@@ -113,9 +110,9 @@ function addCircle(column, matrixIndex) {
     for (let i = column.length - 1; i >= 0; i--) {
         if (column[i].style.backgroundColor !== PLAYER_ONE_COLOR && column[i].style.backgroundColor !== PLAYER_TWO_COLOR) {
             column[i].style.backgroundColor = CURRENT_COLOR ? PLAYER_ONE_COLOR : PLAYER_TWO_COLOR
-            column[i].parentElement.style.backgroundColor = CURRENT_COLOR ? PLAYER_ONE_COLOR : PLAYER_TWO_COLOR
-            column[i].parentElement.style.outline = '1px solid black'
-            column[i].parentElement.style.borderTop = '2px solid rgba(0, 0, 0, 0.5)'
+            column[i].style.outline = '1px solid black'
+            column[i].style.borderTop = '2px solid rgba(0, 0, 0, 0.5)'
+            column[i].firstElementChild.style.backgroundColor = CURRENT_COLOR ? PLAYER_ONE_COLOR : PLAYER_TWO_COLOR
 
             playerTurnSpan.innerText = CURRENT_COLOR ? 'PLAYER 2' : 'PLAYER 1'
 
@@ -124,47 +121,58 @@ function addCircle(column, matrixIndex) {
             }
 
             
-            PLAY_MATRIX[i][matrixIndex] = CURRENT_COLOR ? 'red' : 'yellow'
+            PLAY_MATRIX[i][matrixIndex] = CURRENT_COLOR ? true : false
             console.log(PLAY_MATRIX)
-            checkIfThereIsAWinner()
+            
 
             CURRENT_COLOR = !CURRENT_COLOR
             break;
         }
     }
 
-    startTimer()
+    if (checkIfThereIsAWinner()) {
+        whoWonSpan.innerText = PLAYER_WHO_WON ? "RED" : "YELLOW"
+        whoWonSpan.style.color = PLAYER_WHO_WON ? PLAYER_ONE_COLOR : PLAYER_TWO_COLOR
+        whoWonSpan.style.fontSize = '20px'
+        endingDialog.classList.add('slide-to-middle')
+        endingDialog.showModal()
+    } else startTimer()
 }
 
-insideTheHole.forEach(hole => {
+holes.forEach(hole => {
     hole.addEventListener('click', (e) => {
         let column = ''
+        let holeClicked = e.target
 
-        if (e.target.classList.contains('first-column')) {
+        if (holeClicked.classList.contains('inside-the-hole')) {
+            holeClicked = e.target.parentElement
+        }
+
+        if (holeClicked.classList.contains('first-column')) {
             column = firstColumn
             index = 0
         }
-        else if (e.target.classList.contains('second-column')) {
+        else if (holeClicked.classList.contains('second-column')) {
             column = secondColumn
             index = 1
         }
-        else if (e.target.classList.contains('third-column')) {
+        else if (holeClicked.classList.contains('third-column')) {
             column = thirdColumn
             index = 2
         }
-        else if (e.target.classList.contains('fourth-column')) {
+        else if (holeClicked.classList.contains('fourth-column')) {
             column = fourthColumn
             index = 3
         }
-        else if (e.target.classList.contains('fifth-column')) {
+        else if (holeClicked.classList.contains('fifth-column')) {
             column = fifthColumn
             index = 4
         }
-        else if (e.target.classList.contains('sixth-column')) {
+        else if (holeClicked.classList.contains('sixth-column')) {
             column = sixthColumn
             index = 5
         }
-        else if (e.target.classList.contains('seventh-column')) {
+        else if (holeClicked.contains('seventh-column')) {
             column = seventhColumn
             index = 6
         }
